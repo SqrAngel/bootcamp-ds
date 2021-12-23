5eabb800fd08210c7479c0efd171d81fd7e9b3ff
Joshua prathama raga lawa <joshuaragalawa09@gmail.com>
2021-12-23 13:47:33 +0700

    update session 2

--- session1/code/main.cpp
+++ session1/code/main.cpp
@@ -32,11 +32,14 @@ struct Student
 // Student create(char name[], double GPA, char id[], char namaDosen[]){
 
 // }
-Student* create(char *name, double GPA, char *id, char *namaDosen){
+Student* create(const char *name, double GPA, const char *ID, const char *namaDosen){
     // 6 baris
     Student *newStudent = (Student*)malloc(sizeof(Student));
     strcpy(newStudent->name, name);
-
+    newStudent->GPA = GPA;
+    strcpy(newStudent->c.ID, ID);
+    strcpy(newStudent->c.namaDosen, namaDosen);
+ 
     return newStudent; ///return variable pointer yg menunjuk ke address newStudent
 }
 
@@ -48,6 +51,15 @@ void read(Student *s) { //yang kita pass adalah pointer yangmenunjuk ke alamat s
     printf("Nama Dosen: %s\n", s->c.namaDosen); //(*s).c.namaDosen
 }
 
+void update(Student *s){
+    strcpy(s->name, "Tanto");
+}
+
+void remove(Student *s) {
+    free(s); // freee alocated memory from malloc 
+    s = NULL; //protect against dangling pointer
+}
+
 int main(){
     // Student s = {"Joshua", 3.90, {"COMP6047", "Pak Indra"}};
     // printf("%s ", s.name);
@@ -55,11 +67,17 @@ int main(){
 
     //create("Joshua", 3.90, "COMP6047", "Pak Indra"); // --> ini coding tidak akan jalan karena bukan passing variable
     //caranya kita buat seperti ini 
-    char name[] = "Joshua";
-    char ID[] = "6047";
-    char namaDosen[] = "Pak Indra";
-    create(name, 3.90, ID, namaDosen);
+    // char name[] = "Joshua";
+    // char ID[] = "6047";
+    // char namaDosen[] = "Pak Indra";
     // tapi cara ini tidak efisien karena struct memiliki banyak variable dan data pasti banyak yang akan dimasukan
+    // create(name, 3.90, ID, namaDosen);
+    Student *s = create("Joshua", 3.9, "COMP6047", "Pak Indra");
+    read(s);
+    update(s);
+    // remove(s);
+    read(s);
+    
 
 
 
